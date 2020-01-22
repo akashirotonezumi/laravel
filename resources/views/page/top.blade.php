5c982513119022a8b73d499e36583be23eb50306
sa <58499693+akashirotonezumi@users.noreply.github.com>
2020-01-22 18:42:56 +0900

    Update top.blade.php

--- resources/views/page/top.blade.php
+++ resources/views/page/top.blade.php
@@ -5,15 +5,6 @@
 @section('content')
 <h1>PHP > Laravel</h1>
 <div>
-    <p>
-        Github:
-        <a href="https://github.com/akashirotonezumi/laravel" target="_blank" rel="noopener noreferrer">
-            https://github.com/akashirotonezumi/laravel
-        </a>
-        <svg id="i-external" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="13" height="13" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-            <path d="M14 9 L3 9 3 29 23 29 23 18 M18 4 L28 4 28 14 M28 4 L14 18" />
-        </svg>
-    </p>
     <p>PHP 7.2</p>
     <p>
         <a class="" href="https://readouble.com/laravel/5.5/ja/installation.html" target="_blank" rel="noopener noreferrer">Laravel 5.5</a>
